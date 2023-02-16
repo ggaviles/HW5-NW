eeab37505a10eff3582413b5a868ac2541fc42ec
Giovanni Aviles <giovanni.aviles@ucsf.edu>
2023-02-15 23:57:48 -0800

    Updated .yml file

--- test/test_align.py
+++ test/test_align.py
@@ -8,13 +8,14 @@ def test_nw_alignment():
     TODO: Write your unit test for NW alignment
     using test_seq1.fa and test_seq2.fa by
     asserting that you have correctly filled out
-    the your 3 alignment matrices.
+    your 3 alignment matrices.
     Use the BLOSUM62 matrix and a gap open penalty
     of -10 and a gap extension penalty of -1.
     """
     seq1, _ = read_fasta("./data/test_seq1.fa")
     seq2, _ = read_fasta("./data/test_seq2.fa")
     needle_wunsch = NeedlemanWunsch("./substitution_matrices/BLOSUM62.mat", -10.0, -1.0)
+
 
 
 def test_nw_backtrace():
